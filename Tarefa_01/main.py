61e593cf6129307b3fb2151c4e987a77135ef3c9
Leiriele Correa <leiricorrea@gmail.com>
2023-04-02 17:49:03 -0300

    agregando lematizacao

--- Tarefa_01/main.py
+++ Tarefa_01/main.py
@@ -4,12 +4,21 @@ import PyPDF2
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
+# from nltk.stem import WordNetLemmatizer
 import inflect
 import pandas as pd
+import spacy
+
+# nltk.download('wordnet')
+
+# Criando o objeto lematizador
+# lemmatizer = WordNetLemmatizer()
 
 # Definir a lista de stopwords e pontuações que serão removidas dos documentos
 stop_words = set(stopwords.words('portuguese'))
-stop_words.update(['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}'])
+
+stop_words.update(['.', ',', '"', "'", '?', '!', ':',
+                  ';', '(', ')', '[', ']', '{', '}'])
 
 # Definir o stemmer e o inflector para processar as palavras
 stemmer = SnowballStemmer('portuguese')
@@ -34,11 +43,19 @@ for filename in os.listdir('Docs'):
         # Remover stopwords
         words = [word for word in words if word not in stop_words]
 
+        # Lematizacao nao aceita portugues
+        # words = [lemmatizer.lemmatize(word) for word in words]
+        nlp = spacy.load('pt_core_news_sm')
+        words = [token.lemma_ for token in nlp(' '.join(words))]
+
         # Processar as palavras
-        words = [inflector.singular_noun(stemmer.stem(word)) or word for word in words]
+        words = [inflector.singular_noun(
+            stemmer.stem(word)) or word for word in words]
 
         # Iterar pelas palavras e adicionar as entradas apropriadas ao dicionário de termos e postings
         for word in words:
+            if word in stop_words:
+                continue #pula a iteração atual do loop se a palavra atual for uma stopword.
             if word in terms_dict:
                 if filename in [docid for docid, freq in terms_dict[word]]:
                     for posting in terms_dict[word]:
@@ -53,38 +70,46 @@ for filename in os.listdir('Docs'):
 index_data = []
 for term, postings in sorted(terms_dict.items()):
     freq = sum([freq for filename, freq in postings])
-    postings_list = ', '.join([f'({docid}, {freq})' for docid, freq in postings])
+    postings_list = ', '.join(
+        [f'({docid}, {freq})' for docid, freq in postings])
     index_data.append({'Termo': term, 'Freq': freq, 'Postings': postings_list})
+
 
 # Define um dataframe Pandas para a criação das tabelas
 index_df = pd.DataFrame(index_data)
 print(index_df)
 
-
 # Salvar o Indice Invertido em um relatorio txt
-file_path = 'Relatorios/Relatorio_Indice_Invertido.txt'
+file_path = 'Relatorios/Relatorio_Indice_Invertido2.txt'
+
 # Verificar se o arquivo ainda não existe
 if not os.path.exists(file_path):
     with open(file_path, 'w+') as f:
         f.write('❶ Índice Invertido ❶\n\n')
 
 # Salvar Índice Invertido no relatório
-with open('Relatorios/Relatorio_Indice_Invertido.txt', 'a') as arquivo_saida:
+with open('Relatorios/Relatorio_Indice_Invertido2.txt', 'a') as arquivo_saida:
     for term, postings in terms_dict.items():
         freq = len(postings)
-        arquivo_saida.write('Termo "{}" encontrado em {} documento(s):\n {}\n\n'.format(term, freq, postings))
+        arquivo_saida.write(
+            'Termo "{}" encontrado em {} documento(s):\n {}\n\n'.format(term, freq, postings))
 
-# Salvar as Stopwords em um relatorio txt 
-file_path = 'Relatorios/Relatorio_Stopwords.txt'
+# Salvar as Stopwords em um relatorio txt
+file_path = 'Relatorios/Relatorio_Stopwords2.txt'
 # Verificar se o arquivo ainda não existe
 if not os.path.exists(file_path):
     with open(file_path, 'w+') as f:
         f.write('❶ Stopwords ❶\n\n')
 
 # Salvando Stopwords no relatorio
-with open('Relatorios/Relatorio_Stopwords.txt', 'a') as f:
+with open('Relatorios/Relatorio_Stopwords2.txt', 'a') as f:
     for word in stop_words:
         f.write(word + '\n')
 
+# Salvar a lematização em um arquivo de texto
+with open('lemmatization.txt', 'w') as f:
+    f.write('\n'.join(words))
+
 # Salvar o índice invertido em um relatorio XLSX
-index_df.to_excel('Relatorios/Relatorio_Indice_Invertido_Tabela.xlsx', index=False)
+index_df.to_excel(
+    'Relatorios/Relatorio_Indice_Invertido_Tabela.xlsx', index=False)
